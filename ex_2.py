a6b0385780e01660833bb37780fe90f0c3b47354
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-07-05 20:40:34 +0300

    Create ex_2.py

---
+++ ex_2.py
@@ -0,0 +1,17 @@
+class MobilePhone:
+
+    def __init__(self):
+        print("Creating a phone...")
+
+    def turn_on(self):
+        print("On...")
+
+    def turn_off(self):
+        print("Off...")
+
+
+iphone = MobilePhone()
+samsung = MobilePhone()
+
+samsung.turn_on()
+iphone.turn_on()
